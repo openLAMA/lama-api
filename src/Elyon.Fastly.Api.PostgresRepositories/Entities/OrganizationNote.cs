b4631556e56d38dd6da29351776902e14241343e
Anatoli Angelov <anatoli.angelov@primeholding.com>
2021-05-21 16:05:27 +0300

    Make UserId nullable in Organization Notes to allow deleting user

--- src/Elyon.Fastly.Api.PostgresRepositories/Entities/OrganizationNote.cs
+++ src/Elyon.Fastly.Api.PostgresRepositories/Entities/OrganizationNote.cs
@@ -30,7 +30,7 @@ namespace Elyon.Fastly.Api.PostgresRepositories.Entities
 
         public DateTime CreatedOn { get; set; }
 
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
 
         public User User { get; set; }
 
